d8e18bacdec6137e88d4aef31a2f5ea5e245cc28
Cr7stian8 <cristianotropeiro@gmail.com>
2022-05-10 07:45:24 -0300

    Adição segundo exercício 'doWhile'

---
+++ exercicios/Repetição/doWhile_2.java
@@ -0,0 +1,31 @@
+/*
+ Escrever um programa que receba vários números inteiros no teclado. E no
+final imprimir a média dos números múltiplos de 3. Para sair digitar
+0(zero).
+ */
+package exerciciosHoje;
+
+import java.util.Scanner;
+
+public class doWhile_2 {
+	public static void main(String[] args) {
+		try (Scanner input = new Scanner(System.in)) {
+			// declarando variaveis
+			int numero, multiplo3 = 0, divisor = 0, media;
+			do {
+				//Recebendo o número
+				System.out.println("Digite um número: ");
+				numero = input.nextInt();
+				//Verificando se o número é multiplo de 3
+				if (numero % 3 == 0 && numero != 0) {
+					multiplo3 += numero;
+					divisor++;
+				}
+			} while (numero != 0);
+
+			// Calculando a média dos multiplos de 3
+			media = multiplo3 / divisor;
+			System.out.println("A média dos números multiplos de três é: " + media);
+		}
+	}
+}
