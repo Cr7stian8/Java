e7468f41606debdf486f032438260123aabe2d38
Cr7stian8 <cristianotropeiro@gmail.com>
2022-05-18 15:35:55 -0300

    Renomeando class

--- exercicios/POO/exercicio2/exercicio2.java
+++ exercicios/POO/exercicio2/exercicio2.java
@@ -1,10 +1,10 @@
-package exercicio2;
+package Exercicio2;
 
 public class Exercicio2 {
 
 	public static void main(String[] args) {
 		
-		//criando objeto pertencente a classe aviï¿½o
+		//criando objeto pertencente a classe avião
 		Aviao aviao1 = new Aviao("Latam",20,200);
 		aviao1.aviaoDados();
 	}
